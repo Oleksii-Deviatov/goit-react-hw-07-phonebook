459b312a7272c9ad8bc9044a3ffd5b8fd20bbd50
lewyk <lewyk@hotmail.com>
2021-04-07 16:42:37 +0300

    add remove storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,12 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contacts/contacts-reducer';
 
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-];
-
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
+const middleware = [...getDefaultMiddleware()];
 
 const store = configureStore({
-  reducer: persistReducer(contactsPersistConfig, contactsReducer),
+  reducer: contactsReducer,
   middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export { store };
